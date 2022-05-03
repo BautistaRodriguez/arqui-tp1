2056c4a6c03015d842f05fcf4eb6527337ea5299
bauti <jbrodriguez@fi.uba.ar>
2022-05-03 19:42:00 -0300

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
+});
+
+app.route('/article')
+.get(function (req, res) {
+    res.send('Get the article');
+})
+.post(function (req, res) {
+    res.send('Add an article');
+})
+.put(function (req, res) {
+    res.send('Update the article');
+});
+
+app.get('/the*man', function (req, res) {
+    res.send('the*man');
+});
+
+app.get(/bat/, function (req, res) {
+    res.send('/bat/');
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("Lo siento, esa ruta no existe. Que tengas un buen dia :)");
+});
+
+app.listen(3000, function () {
+    console.log('Aplicacion de ejemplo escuchando en el puerto 3000.');
+});
